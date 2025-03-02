8428eaa1fa9840407a2707e2076ae5814d54a28d
김원태 <rladnjsxo123@naver.com>
2025-03-02 16:58:46 +0900

    188 fetch 캐시 옵션들

--- app/messages/page.js
+++ app/messages/page.js
@@ -1,12 +1,14 @@
+import { unstable_noStore } from "next/cache";
+
 import Messages from "@/components/messages";
 
+// export const revalidate = 5; // 5초마다 갱신
+
+// export const dynamic = "force-dynamic"; // cache : no-store와 같음 .
+
 export default async function MessagesPage() {
-  const response = await fetch("http://localhost:8080/messages", {
-    next: {
-      revalidate: 5,
-    },
-    // cache : 'no-cache' // 'no-store'
-  });
+  unstable_noStore();
+  const response = await fetch("http://localhost:8080/messages");
   const messages = await response.json();
 
   if (!messages || messages.length === 0) {
