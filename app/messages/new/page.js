d93728ad924f29b00c4c51de6d7e7b59c7fe2db0
김원태 <rladnjsxo123@naver.com>
2025-03-02 17:15:36 +0900

    191 커스텀 데이터 소스에 대한 요청 메모화 설정

--- app/messages/new/page.js
+++ app/messages/new/page.js
@@ -1,7 +1,6 @@
 import { redirect } from "next/navigation";
 
 import { addMessage } from "@/lib/messages";
-import { revalidateTag } from "next/cache";
 
 export default function NewMessagePage() {
   async function createMessage(formData) {
@@ -9,7 +8,8 @@ export default function NewMessagePage() {
 
     const message = formData.get("message");
     addMessage(message);
-    revalidateTag("msg");
+    // revalidateTag("msg");
+
     redirect("/messages");
   }
 
--- app/messages/page.js
+++ app/messages/page.js
@@ -1,17 +1,18 @@
-import { unstable_noStore } from "next/cache";
-
 import Messages from "@/components/messages";
+import { getMessages } from "@/lib/messages";
 
 // export const revalidate = 5; // 5초마다 갱신
 
 // export const dynamic = "force-dynamic"; // cache : no-store와 같음 .
 
-export default async function MessagesPage() {
-  unstable_noStore();
-  const response = await fetch("http://localhost:8080/messages", {
-    next: { tags: ["msg"] },
-  });
-  const messages = await response.json();
+export default function MessagesPage() {
+  // unstable_noStore();
+  // const response = await fetch("http://localhost:8080/messages", {
+  //   next: { tags: ["msg"] },
+  // });
+  // const messages = await response.json();
+
+  const messages = getMessages();
 
   if (!messages || messages.length === 0) {
     return <p>No messages found</p>;
