58074d8162e8c69865f8bb571dea3b2b54ec403c
김원태 <rladnjsxo123@naver.com>
2025-03-02 17:23:10 +0900

    193커스텀 데이터 소스 데이터 무효화

--- app/messages/new/page.js
+++ app/messages/new/page.js
@@ -1,6 +1,7 @@
 import { redirect } from "next/navigation";
 
 import { addMessage } from "@/lib/messages";
+import { revalidateTag } from "next/cache";
 
 export default function NewMessagePage() {
   async function createMessage(formData) {
@@ -8,8 +9,7 @@ export default function NewMessagePage() {
 
     const message = formData.get("message");
     addMessage(message);
-    // revalidateTag("msg");
-
+    revalidateTag("msg");
     redirect("/messages");
   }
 
